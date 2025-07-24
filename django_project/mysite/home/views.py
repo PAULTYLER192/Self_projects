2f8dd1e40f3424802788def60d135e36e1c2809a
PAULTYLER192 <ochiengpaul193@gmail.com>
2025-07-24 15:06:58 +0300

    updated navigation links by linking local urls

--- django_project/mysite/home/views.py
+++ django_project/mysite/home/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request):
   return render(request,'home/index.html')
+  
+def about_view(request):
+  return render(request,'home/about.html')
+  
+def contact_view(request):
+  return render(request,'home/contact.html')
