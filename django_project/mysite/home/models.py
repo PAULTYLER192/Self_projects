0f2da6cd43ba18e623d7d6f15da6cedead492853
PAULTYLER192 <ochiengpaul193@gmail.com>
2025-07-27 17:25:04 +0300

    updated the post model by adding author  attribute

--- django_project/mysite/home/models.py
+++ django_project/mysite/home/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    author = models.CharField(max_length=90, null=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
